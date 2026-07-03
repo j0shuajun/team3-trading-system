f206ea1b67590a7122156805b844205c7e43fdfb
j0shuajun <kimmj1e34@naver.com>
2026-07-03 14:12:07 +0900

    fix select_stock_broker

--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -3,55 +3,53 @@ from mock_stock_broker_driver import MockStockBrokerDriver
 
 
 def test_login():
-    driver = MockStockBrokerDriver()
     system = AutoTradingSystem()
 
-    system.select_stock_broker(driver)
+    system.select_stock_broker("mock")
     system.login("user.id", "password")
 
+    driver = system.stock_broker_driver
     assert driver.login_id == "user.id"
     assert driver.login_password == "password"
 
 
 def test_select_stock_broker():
-    driver = MockStockBrokerDriver()
     system = AutoTradingSystem()
 
-    system.select_stock_broker(driver)
+    system.select_stock_broker("mock")
 
-    assert system.stock_broker_driver == driver
+    assert isinstance(system.stock_broker_driver, MockStockBrokerDriver)
 
 
 def test_buy_stock():
-    driver = MockStockBrokerDriver()
     system = AutoTradingSystem()
 
-    system.select_stock_broker(driver)
+    system.select_stock_broker("mock")
     system.buy("005930", 70000, 3)
 
+    driver = system.stock_broker_driver
     assert driver.buy_stock_code == "005930"
     assert driver.buy_price == 70000
     assert driver.buy_quantity == 3
 
 
 def test_sell_stock():
-    driver = MockStockBrokerDriver()
     system = AutoTradingSystem()
 
-    system.select_stock_broker(driver)
+    system.select_stock_broker("mock")
     system.sell("000660", 180000, 2)
 
+    driver = system.stock_broker_driver
     assert driver.sell_stock_code == "000660"
     assert driver.sell_price == 180000
     assert driver.sell_quantity == 2
 
 
 def test_get_price():
-    driver = MockStockBrokerDriver()
     system = AutoTradingSystem()
-    driver.set_prices("005930", [70000])
 
-    system.select_stock_broker(driver)
+    system.select_stock_broker("mock")
+    system.stock_broker_driver.set_prices("005930", [70000])
     price = system.get_price("005930")
 
     assert price == 70000
