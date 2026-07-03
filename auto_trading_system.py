7f089a3abd5c8a97050334222745a7d5ee99763e
yejinlee <progress_6@naver.com>
2026-07-03 15:57:18 +0900

    refactor: trend에 따라 last price를 구하는 함수 작성

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -5,21 +5,21 @@ from stub_auto_trading_system import StubAutoTradingSystem
 
 class AutoTradingSystem(StubAutoTradingSystem):
     def buy_nice_timing(self, stock_code, amount):
-        price = self._stocker_broker.get_price(stock_code)
-        for _ in range(2):
-            current_price = self._stocker_broker.get_price(stock_code)
-            if price >= current_price:
-                return
-            price = current_price
-            time.sleep(0.2)
-        self.buy(stock_code, price, amount // price)
+        price = self._get_last_price_when_rising_or_fall_trend(stock_code, 'rising')
+        if price is not None: self.buy(stock_code, price, amount // price)
 
     def sell_nice_timing(self, stock_code, quantity):
+        price = self._get_last_price_when_rising_or_fall_trend(stock_code, 'fall')
+        if price is not None: self.sell(stock_code, price, quantity)
+
+    def _get_last_price_when_rising_or_fall_trend(self, stock_code, trend: str) -> int:
         price = self._stocker_broker.get_price(stock_code)
         for _ in range(2):
             current_price = self._stocker_broker.get_price(stock_code)
-            if price <= current_price:
+            if trend == "rising" and price >= current_price:
+                return
+            if trend == "fall" and price <= current_price:
                 return
             price = current_price
             time.sleep(0.2)
-        if price is not None: self.sell(stock_code, price, quantity)
+        return price
