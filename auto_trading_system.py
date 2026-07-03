6eeffadbb47dc000e4c2cfd771007a9c7a754866
yejinlee <progress_6@naver.com>
2026-07-03 16:03:00 +0900

    chore: stub class 제거

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,9 +1,7 @@
 import time
 
-from stub_auto_trading_system import StubAutoTradingSystem
 
-
-class AutoTradingSystem(StubAutoTradingSystem):
+class AutoTradingSystem:
     def buy_nice_timing(self, stock_code, amount):
         price = self._get_last_price_when_rising_or_fall_trend(stock_code, 'rising')
         if price is not None: self.buy(stock_code, price, amount // price)
@@ -12,7 +10,7 @@ class AutoTradingSystem(StubAutoTradingSystem):
         price = self._get_last_price_when_rising_or_fall_trend(stock_code, 'fall')
         if price is not None: self.sell(stock_code, price, quantity)
 
-    def _get_last_price_when_rising_or_fall_trend(self, stock_code, trend: str) -> int:
+    def _get_last_price_when_rising_or_fall_trend(self, stock_code, trend: str):
         price = self._stocker_broker.get_price(stock_code)
         for _ in range(2):
             current_price = self._stocker_broker.get_price(stock_code)
