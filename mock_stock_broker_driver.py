6aaa7277547e6fbc7bed106c979825db037c8608
soyeongk <soyeong_k@naver.com>
2026-07-03 13:23:51 +0900

    feat: mock login 구현

--- mock_stock_broker_driver.py
+++ mock_stock_broker_driver.py
@@ -3,3 +3,7 @@ class MockStockBrokerDriver():
     def __init__(self):
         self.login_id = ""
         self.login_password = ""
+
+    def login(self, login_id, login_password):
+        self.login_id = login_id
+        self.login_password = login_password
