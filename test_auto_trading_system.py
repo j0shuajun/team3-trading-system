a2dedc0fbef5450be7a1b68d31ef515ee6081b1f
j0shuajun <kimmj1e34@naver.com>
2026-07-03 14:01:13 +0900

    unit tests for auto trading system

---
+++ test_auto_trading_system.py
@@ -0,0 +1,57 @@
+from auto_trading_system import AutoTradingSystem
+from mock_stock_broker_driver import MockStockBrokerDriver
+
+
+def test_login():
+    driver = MockStockBrokerDriver()
+    system = AutoTradingSystem()
+
+    system.select_stock_broker(driver)
+    system.login("user.id", "password")
+
+    assert driver.login_id == "user.id"
+    assert driver.login_password == "password"
+
+
+def test_select_stock_broker():
+    driver = MockStockBrokerDriver()
+    system = AutoTradingSystem()
+
+    system.select_stock_broker(driver)
+
+    assert system.stock_broker_driver == driver
+
+
+def test_buy_stock():
+    driver = MockStockBrokerDriver()
+    system = AutoTradingSystem()
+
+    system.select_stock_broker(driver)
+    system.buy("005930", 70000, 3)
+
+    assert driver.buy_stock_code == "005930"
+    assert driver.buy_price == 70000
+    assert driver.buy_quantity == 3
+
+
+def test_sell_stock():
+    driver = MockStockBrokerDriver()
+    system = AutoTradingSystem()
+
+    system.select_stock_broker(driver)
+    system.sell("000660", 180000, 2)
+
+    assert driver.sell_stock_code == "000660"
+    assert driver.sell_price == 180000
+    assert driver.sell_quantity == 2
+
+
+def test_get_price():
+    driver = MockStockBrokerDriver()
+    system = AutoTradingSystem()
+    driver.set_prices("005930", [70000])
+
+    system.select_stock_broker(driver)
+    price = system.get_price("005930")
+
+    assert price == 70000
