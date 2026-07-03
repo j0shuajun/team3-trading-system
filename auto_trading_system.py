2e6d45929f9d6873d06c0bb399c22775f4b4951a
jinwoo <jinwoo.develop@gmail.com>
2026-07-03 13:41:43 +0900

    feat: AutoTradingSystem 로그 추가 및 stockBrocker 구분

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,18 +1,44 @@
+from enum import Enum
+
+
+class StockerBroker(Enum):
+    KiwerStock = "kiwer"
+    NemoStock = "nemo"
+    MockStock = "mock"
+
+
 class AutoTradingSystem:
     def __init__(self):
         self._stocker_broker = None
 
-    def select_stocker_broker(self, name:str):
-        ...
+    def select_stocker_broker(self, name: str):
+        if name == StockerBroker.KiwerStock.value:
+            pass
+        if name == StockerBroker.NemoStock.value:
+            pass
+        if name == StockerBroker.MockStock.value:
+            pass
 
-    def login(self, id:str, pw:str):
-        ...
+    def login(self, id: str, pw: str):
+        if len(id) == 0 or len(pw) == 0:
+            raise ValueError
 
-    def buy(self, code:str, price:str, cnt:str):
-        ...
+        print(f"{id} 로그인 시도..")
 
-    def sell(self, code:str, price:str, cnt:str):
-        ...
+    def buy(self, code: str, price: int, cnt: int):
+        if len(code) == 0 or price <= 0 or cnt <= 0:
+            raise ValueError
 
-    def get_price(self, code:str):
-        ...
+        print(f"{code} 종목 {price}원*{cnt}주 매수 시도..")
+
+    def sell(self, code: str, price: int, cnt: int):
+        if len(code) == 0 or price <= 0 or cnt <= 0:
+            raise ValueError
+
+        print(f"{code} 종목 {price}원*{cnt}주 매도 시도..")
+
+    def get_price(self, code: str):
+        if len(code) == 0:
+            raise ValueError
+
+        print(f"{code} 종목 가격 확인..")
